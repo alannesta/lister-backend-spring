3f1efa1bb4350dfa2dcf4ea3589a73b3e2745576
Alan Cao <alannesta@sina.com>
2018-05-23 09:20:29 -0400

    jwt filter integration

--- src/main/java/fetcher/security/SecurityConfiguration.java
+++ src/main/java/fetcher/security/SecurityConfiguration.java
@@ -2,33 +2,29 @@ package fetcher.security;
 
 import fetcher.security.filters.JwtFilter;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.password.Pbkdf2PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Autowired
-    private UserDetailsImpl userDetailsImpl;
-    @Autowired
     CustomAuthenticationProvider customAuthenticationProvider;
+    @Autowired
+    JwtFilter jwtFilter;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.cors().and().csrf().disable().authorizeRequests()
                 .antMatchers("/login/**").permitAll()
-                .antMatchers("/movies/**").permitAll()
+//                .antMatchers("/movies/**").permitAll()
                 .anyRequest().authenticated()
                 .and()
-                .addFilterBefore(new JwtFilter(), UsernamePasswordAuthenticationFilter.class);
-//                .addFilter(new JWTAuthorizationFilter(authenticationManager()));
+                .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
     }
 
     @Override
--- src/main/java/fetcher/security/filters/JwtFilter.java
+++ src/main/java/fetcher/security/filters/JwtFilter.java
@@ -4,22 +4,24 @@ import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.JwtException;
 import io.jsonwebtoken.Jwts;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.catalina.servlet4preview.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
 import org.springframework.web.filter.GenericFilterBean;
 
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
 @Slf4j
+@Component
 public class JwtFilter extends GenericFilterBean {
     @Value("${security.jwt.secret}")
     private String secretKey;
@@ -47,7 +49,7 @@ public class JwtFilter extends GenericFilterBean {
         if (token != null) {
             try {
                 Claims claim = Jwts.parser()
-                        .setSigningKey(secretKey.getBytes())
+                        .setSigningKey(secretKey)
                         .parseClaimsJws(token)
                         .getBody();
                 if (claim.get("username") != null) {
