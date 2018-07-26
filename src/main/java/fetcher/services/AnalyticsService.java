73759ea3a393c2e3da7172201a356a894d577476
alannesta <alannesta@sina.com>
2018-07-26 17:39:31 -0400

    enable scheduler

--- src/main/java/fetcher/services/AnalyticsService.java
+++ src/main/java/fetcher/services/AnalyticsService.java
@@ -2,23 +2,32 @@ package fetcher.services;
 
 import fetcher.analytics.AnalyticsUtil;
 import fetcher.models.UserFavorite;
+import lombok.extern.slf4j.Slf4j;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
 import java.util.Collections;
 import java.util.List;
 
+@Slf4j
 @Service
 public class AnalyticsService {
 
     @Autowired
     AnalyticsUtil analyticsUtil;
 
+    // @Scheduled(fixedRate = 5000)
+    @Scheduled(cron = "0 12 3 */3 * *")
     public List<UserFavorite> getUserFavoriteReport() {
         try {
 
             List<UserFavorite> reports = analyticsUtil.getUserFavoriteReport();
             //printResponse(response);
+            for (UserFavorite userFavorite : reports) {
+                System.out.println(userFavorite.getMovieName());
+            }
             return reports;
         } catch (Exception e) {
             e.printStackTrace();
