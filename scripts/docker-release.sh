86468240540bcbe6d4ca0c030b660566433cf3f1
Alan Cao <alannesta@sina.com>
2018-09-08 10:50:25 -0400

    update docker image

--- scripts/docker-release.sh
+++ scripts/docker-release.sh
@@ -1,22 +1,21 @@
 #!/usr/bin/env bash
+# this script will be run by `maven package` job
 set -ex
 
-#echo $version
-echo "Annie"
-echo $imageVersion > test
+echo $imageVersion
 
-#USERNAME=alannesta
-#IMAGE=reporting-service
-#VERSION=`cat ./DOCKER_IMAGE_VERSION`
+USERNAME=alannesta
+IMAGE=reporting-service
+VERSION=`cat ./DOCKER_IMAGE_VERSION`
 
-#echo "login into docker hub..."
-#docker login --username=alannesta
-#
-#echo "building $USERNAME/$IMAGE:$VERSION..."
-#
-#docker build --build-arg JAR_FILE=target/lister-report-${VERSION}.jar -t $USERNAME/$IMAGE:$VERSION -t $USERNAME/$IMAGE:latest .
-#
-#docker push $USERNAME/$IMAGE:latest
-#docker push $USERNAME/$IMAGE:$VERSION
-#
-#echo "image ${USERNAME}/${IMAGE}:${VERSION} published..."
+echo "login into docker hub..."
+docker login --username=alannesta
+
+echo "building $USERNAME/$IMAGE:$VERSION..."
+
+docker build --build-arg JAR_FILE=target/lister-report-${imageVersion}.jar -t $USERNAME/$IMAGE:$imageVersion -t $USERNAME/$IMAGE:latest .
+
+docker push $USERNAME/$IMAGE:latest
+docker push $USERNAME/$IMAGE:$imageVersion
+
+echo "image ${USERNAME}/${IMAGE}:${imageVersion} published..."
--- src/main/java/fetcher/configuration/CacheConfiguration.java
+++ src/main/java/fetcher/configuration/CacheConfiguration.java
@@ -1,19 +1,28 @@
 package fetcher.configuration;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
+import org.springframework.util.Assert;
 
 @Configuration
 @EnableCaching
 public class CacheConfiguration {
+    @Value("${spring.cache.redis.host}")
+    private String redisHost;
+    @Value("${spring.cache.redis.port}")
+    private int redisPort;
+
     @Bean
     JedisConnectionFactory jedisConnectionFactory() {
-        //RedisStandaloneConfiguration config = new RedisStandaloneConfiguration("localhost", 6379);
-        return new JedisConnectionFactory();
+        Assert.notNull(redisHost, "Redis host needs to be configured in application properties");
+        RedisStandaloneConfiguration config = new RedisStandaloneConfiguration(redisHost, redisPort);
+        return new JedisConnectionFactory(config);
     }
 
     @Bean
